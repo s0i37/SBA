5a96b82bda265b1c30b42635798577befa8bcda3
s0i37 <s0i37@work>
2018-05-18 18:08:30 +0500

    added tiny r2-scripts

---
+++ radare2/prefix/bin/snow.py
@@ -0,0 +1,14 @@
+import r2pipe
+from pygments import highlight
+from pygments.lexers import CppLexer
+from pygments.formatters import Terminal256Formatter
+from os import popen
+
+r2 = r2pipe.open()
+filepath = r2.cmdj("ij")['core']['file']
+from_addr = r2.cmd("?v $FB")
+to_addr = r2.cmd("?v $FE")
+
+nocode = popen( "nocode --from=%s --to=%s %s" % (from_addr, to_addr, filepath) )
+
+print highlight( nocode.read(), CppLexer(), Terminal256Formatter(style='monokai') )
