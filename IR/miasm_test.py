562014fab70d9b282ca866f2d245fa253581d95e
soier <soier@home>
2018-12-25 22:13:38 +0500

    added rw-anal in IR/miasm

--- IR/miasm_test.py
+++ IR/miasm_test.py
@@ -3,13 +3,14 @@ from miasm2.analysis.machine import Machine
 
 opcode1 = 'b837130000'.decode('hex')
 opcode2 = '83c077'.decode('hex')
-opcode3 = '50'.decode('hex')
+opcode3 = '0F84FC000000'.decode('hex')
 
 machine = Machine('x86_32')
 
 def IR(opcode):
 	loc_db = LocationDB()
 	instr = machine.mn.dis(opcode, 32)
+	print instr
 
 	ira = machine.ira(loc_db)
 	ircfg = ira.new_ircfg()
@@ -17,7 +18,12 @@ def IR(opcode):
 
 	for lbl, irblock in ircfg.blocks.items():
 		print irblock.to_string(loc_db)
-
+		for assignblk in irblock:
+			rw = assignblk.get_rw()
+			for writes, reads in rw.iteritems():
+				print '\tread: ', ','.join( [str(x) for x in reads] )
+				print '\twrite: ', writes
+	print ''
 
 IR(opcode1)
 IR(opcode2)
