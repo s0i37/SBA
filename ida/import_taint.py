13e75667fc61b37b9af2be6f9158c6bc35fa6a76
s0i37 <s0i37@work>
2019-01-11 14:35:07 +0500

    added simple taint visualisation in IDA

---
+++ ida/import_taint.py
@@ -0,0 +1,60 @@
+#from idaapi import *
+#from idautils import *
+#from idc import *
+
+RED = 0xaaaaff
+GREEN = 0xaaffaa
+GREEN_DARK = 0x22aa22
+CYAN = 0xdddd00
+BLUE = 0xffaaaa
+YELLOW = 0x00ffff
+GREY = 0xbbbbbb
+WHITE = 0xffffff
+
+TAINT_LOG = 'z:/root/taint.log'
+
+def colorize_taint(addrs_file, color, prefix=''):
+	function_names = []
+	commented = set()
+	with open( addrs_file, "rb") as f:
+		for line in f:
+			try:
+				eip = int( line.split(';')[0].split(':')[2], 16)
+				comment = line.split(';')[1].strip()
+
+				function_name = GetFunctionName(eip)
+				if not function_name in function_names:
+					function_names.append(function_name)
+				SetColor( eip, CIC_ITEM, color )
+				
+				if comment:
+					if not eip in commented:
+						set_cmt( eip, comment, 0 )
+						commented.add(eip)
+					else:
+						#set_cmt( eip, GetCommentEx(eip, 0) + '\n' + comment, 0 )
+						pass
+			except Exception as e:
+				print str(e)
+				pass
+
+	if prefix:
+		num = 0
+		new_function_names = []
+		for function_name in function_names:
+			try:
+				new_function_name = "{prefix}_{num}_{func}".format( prefix=prefix, num=num, func=function_name )
+				MakeName( LocByName(function_name), new_function_name )
+				new_function_names.append(new_function_name)
+			except:
+				pass
+			num += 1
+		function_names = new_function_names
+
+	print "tainted %d functions:" % len(function_names)
+	for function_name in function_names:
+		print function_name
+
+
+#colorize_taint( TAINT_LOG, color=YELLOW, prefix='taint' )
+colorize_taint( TAINT_LOG, color=YELLOW )
