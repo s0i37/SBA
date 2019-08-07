e533658214fa0943f0a3b248fffe03c9f6d03b65
s0i37 <s0i37@work>
2019-08-07 16:54:26 +0500

    added a few examples for parsing ELF

--- elffile_test.py
+++ elffile_test.py
@@ -1,5 +1,6 @@
 from elftools.elf.elffile import ELFFile
 from elftools.elf.relocation import RelocationSection
+import lief
 from capstone import *
 from sys import argv
 
@@ -23,3 +24,24 @@ def disas(elf):
 
 elf = ELFFile( open(argv[1], "rb") )
 enum_sections(elf)
+
+elf = lief.parse( argv[1] )
+elf.has_nx
+elf.is_pie
+elf.header.entrypoint
+for section in elf.sections:
+	print "%s %d" % (section.name, section.size)
+bytes( elf.get_section('.text').content )
+
+elf.patch_address(0x115D, bytearray(b"\x90\x90"))
+elf.write("/tmp/patched")
+
+section = lief.ELF.Section(".newsection", lief.ELF.SECTION_TYPES.PROGBITS)
+section += lief.ELF.SECTION_FLAGS.EXECINSTR
+section += lief.ELF.SECTION_FLAGS.ALLOC
+section.content = bytearray("\x90"*16)  
+elf.add(section, loaded=True)
+elf.write("/tmp/patched2")
+
+elf.remove_section('.newsection')
+elf.write("/tmp/patched3")
