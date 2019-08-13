8dcd1c6b07787fed47e6041b3d4e0d105a35f43b
s0i37 <s0i37@work>
2019-08-13 17:46:33 +0500

    changed memory dump plugin for r2

--- radare2/prefix/bin/memdump.py
+++ radare2/prefix/bin/memdump.py
@@ -4,6 +4,6 @@ import r2pipe
 r2 = r2pipe.open()
 
 for section in r2.cmdj("iSj"):
-	dumpfile_name = "{section}={vaddr}={perm}".format( section=section["name"], vaddr=hex(section["vaddr"]), perm=section["perm"] )
+	dumpfile_name = "{vaddr}={perm}={section}".format( section=section["name"], vaddr="0x%08x"%section["vaddr"], perm=section["perm"] )
 	r2.cmd("s {vaddr}; wtf {dumpfile} @$S!$SS".format( dumpfile=dumpfile_name, vaddr=section["vaddr"] ) )
 	
