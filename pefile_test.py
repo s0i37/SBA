9f5fc9845a12bcc801bfb0c80b96432098835edf
s0i37 <s0i37@work>
2019-10-18 17:00:57 +0500

    a bit changes in pefile_test.py

--- pefile_test.py
+++ pefile_test.py
@@ -12,3 +12,12 @@ base = int( argv[2], 16 ) if len(argv) > 2 else pe.OPTIONAL_HEADER.ImageBase
 
 for sym in pe.DIRECTORY_ENTRY_EXPORT.symbols:
 	print "0x%x %s" % ( base + sym.address, sym.name )
+
+for section in pe.sections:
+	print "%s 0x%x" % ( section.Name.strip(), pe.OPTIONAL_HEADER.ImageBase + section.VirtualAddress )
+	#print section
+	if section.IMAGE_SCN_MEM_EXECUTE:
+		opcodes = section.get_data()
+		offset = section.PointerToRawData + opcodes.find("\x90\x90\x90\x90\x90\x90\x90")
+		pe.set_bytes_at_offset(offset, bytes(_bytes))
+		pe.write("_%s" % module_name)
