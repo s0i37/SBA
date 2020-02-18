fd5d546c10d1ae64f04ed67b587a5b80a53b409f
soier <soier@work>
2020-02-18 06:48:32 +0500

    r2 memdump for core file

--- radare2/prefix/bin/memdump.py
+++ radare2/prefix/bin/memdump.py
@@ -3,7 +3,13 @@ import r2pipe
 
 r2 = r2pipe.open()
 
-for section in r2.cmdj("iSj"):
-	dumpfile_name = "{vaddr}={perm}={section}".format( section=section["name"], vaddr="0x%08x"%section["vaddr"], perm=section["perm"] )
-	r2.cmd("s {vaddr}; wtf {dumpfile} @$S!$SS".format( dumpfile=dumpfile_name, vaddr=section["vaddr"] ) )
-	
+if r2.cmdj("iSj"):
+	#minidump
+	for section in r2.cmdj("iSj"):
+		dumpfile_name = "{vaddr}={perm}={section}".format( section=section["name"], vaddr="0x%08x"%section["vaddr"], perm=section["perm"] )
+		r2.cmd("s {vaddr}; wtf {dumpfile} @$S!$SS".format( dumpfile=dumpfile_name, vaddr=section["vaddr"] ) )
+else:
+	#corefile
+	for page in r2.cmdj("omj"):
+		dumpfile_name = "{vaddr}={perm}={page}".format( page=page["name"].replace('/','-'), vaddr="0x%08x"%page["from"], perm=page["perm"] )
+		r2.cmd("wtf {dumpfile} @{vaddr}!{len}".format( dumpfile=dumpfile_name, vaddr=page["from"], len=(page["to"]-page["from"]) ) )
